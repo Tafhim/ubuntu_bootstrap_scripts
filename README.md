f6949b3734cb951ba1a34b09cf6073d6945ad8fc
Tafhim <tafhim.ul.islam@gmail.com>
2014-11-08 20:12:53 +0600

    Add flash installer support

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Java, Flash auto installation zsh scripts targeted for Ubuntu
 2. Invoke the script with sudo like 
  sudo zsh install_java.sh user /dir/of/java/tar/file
 
+### Flash installation
+1. Download the tar from the Adboe site
+2. Call the script in bash / zsh using
+  sudo zsh install_flash.sh user
+
+
--- install_flash_player.sh
+++ install_flash_player.sh
@@ -1,12 +1,44 @@
-# 		WARNING: Unfinished script with su command			#
 # Script to install Flash player in Ubuntu
-if [ ! -f $PWD/install_flash_player*.tar.gz ]
-	then echo "Place the downloaded Flash player tar.gz in the same dir as this script";
+
+# Check if a user was specified
+USER=$1
+if [ "$USER" = "" ]; then
+  echo "Please specify a user"
+  exit
 fi
-rm -rf flash_player
-mkdir flash_player
-tar xf install_flash_player_11_linux.x86_64.tar.gz -C flash_player
-#cd flash_player
-sudo cp -r flash_player/usr/* /usr
-#mkdir ~/.mozilla/plugins
-sudo cp flash_player/libflashplayer.so ~/.mozilla/plugins/
+
+# Locate the user's home directory
+USER_HOME=/home/$USER
+if [ ! -d $USER_HOME ]; then
+  echo "The user's directory could not be found"
+  exit
+fi
+
+# Assume that the Flash file was downloaded in the downloads folder
+FLASH_FILE_DIR=$USER_HOME/Downloads
+if [ "$2" = "" ]; then
+  echo "You did not specify a location for flash tar files, using $FLASH_FILE_DIR"
+else
+  echo "Looking in $2 for flash files"
+  FLASH_FILE_DIR=$2
+fi
+
+for flashtar in $(ls $FLASH_FILE_DIR | grep install_flash_player); do
+
+  tar_location=$FLASH_FILE_DIR/$flashtar
+
+  echo "Extracting $tar_location.."
+  temp_files_dir=$FLASH_FILE_DIR/flash_install_temp_files
+  mkdir -p $temp_files_dir
+  tar -xf $tar_location -C $temp_files_dir
+  cp -r $temp_files_dir/usr/* /usr/
+
+  if [ -d $USER_HOME/.mozilla/plugins ]; then
+    mkdir -p $USER_HOME/.mozilla/plugins
+  fi
+  cp $temp_files_dir/libflashplayer.so $USER_HOME/.mozilla/plugins
+
+  # Delete the temporary directory
+  rm -rf $temp_files_dir
+
+done
